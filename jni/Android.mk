d2862a9a148e7423a5bb5538eea0e8d8f8274268
hyqs <34120964+hyqs@users.noreply.github.com>
2022-12-24 18:13:42 +0800

    支持Android运行
    LOCAL_LDFLAGS += -pie -fPIE
    LOCAL_CFLAGS += -pie -fPIE

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,5 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vbmeta-disable-verification
 LOCAL_SRC_FILES := main.c
-LOCAL_LDFLAGS := -static
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 include $(BUILD_EXECUTABLE)
